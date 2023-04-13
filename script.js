22e3a80b15baef65f26c9e8c45bfbbf4554b0ed3
lovish <lovishduggal11@gmail.com>
2023-04-13 12:31:03 +0530

    secondDay

--- script.js
+++ script.js
@@ -1,32 +1,102 @@
 const addBtn = document.querySelector('.add-btn');
+const removeBtn = document.querySelector('.remove-btn');
 const modalBox = document.querySelector('.modal-cont');
 const mainContainer = document.querySelector('.main-cont');
-let flag = false;
+let addFlag = false;
+let removeFlag = false;
+const allPriorityColors = document.querySelectorAll('.priority-color');
+const colors = ['lightpink', 'lightblue', 'lightgreen', 'black'];
+let modalPriorityColor = colors[colors.length - 1];
+const textAreaCont = document.querySelector('.text-area-cont');
+//* Listener for modal priority coloring :
+allPriorityColors.forEach((colorElem, idx) => {
+    colorElem.addEventListener('click', (e) => {
+        allPriorityColors.forEach((colorElem, idx) => {
+            colorElem.classList.remove('border');
+        });
+        colorElem.classList.add('border');
+        modalPriorityColor = colorElem.classList[0];
+    });
+});
 addBtn.addEventListener('click', (e) => {
-    flag = !flag;
-    if (flag) {
+    // * Display Modal
+    addFlag = !addFlag; //* we toggle the add button. when we clicked on addBtn modal is generated and again when we clicked on addBtn modal is removed.
+    if (addFlag) {
         modalBox.style.display = 'flex';
     } else {
         modalBox.style.display = 'none';
     }
 });
+//* Toggle the remoce btn
+removeBtn.addEventListener('click', () => {
+    removeFlag = !removeFlag;
+});
+
+//* when shift key is press on modal Box then following logic happens
 modalBox.addEventListener('keydown', (e) => {
     if (e.key === 'Shift') {
-        createTicket();
+        createTicket(modalPriorityColor, textAreaCont.value, shortid());
         modalBox.style.display = 'none';
         modalBox.querySelector('textarea').value = '';
-        flag = false;
+        addFlag = false;
     }
 });
-function createTicket() {
+//* createTicket() is for generate a ticket.
+function createTicket(modalPriorityColor, ticketTask, taskId) {
     const div = document.createElement('div');
     div.setAttribute('class', 'ticket-cont');
     div.innerHTML = `
-        <div class="ticket-cont">
-        <div class="ticket-color"></div>
-        <div class="ticket-id">#sample_id123</div>
-        <div class="task-area"></div>
-        </div>
-    `;
+        <div class="ticket-color  ${modalPriorityColor}"></div>
+        <div class="ticket-id">${taskId}</div>
+        <div class="task-area">${ticketTask}</div>
+        <div class="ticket-lock"><i class="fa-solid fa-lock"></i></div>
+ `;
     mainContainer.append(div);
+    handleRemove(div);
+    handleLock(div);
+    handleColerPriority(div);
+}
+function handleRemove(ticketCont) {
+    ticketCont.addEventListener('click', () => {
+        if (removeFlag) {
+            ticketCont.remove();
+        }
+    });
+}
+function handleLock(ticketCont) {
+    const iEle = ticketCont.querySelector('i');
+    const textArea = ticketCont.querySelector('.task-area');
+    iEle.addEventListener('click', () => {
+        if (iEle.classList.contains('fa-lock')) {
+            iEle.classList.remove('fa-lock');
+            iEle.classList.add('fa-lock-open');
+            textArea.setAttribute('contenteditable', 'true');
+        } else {
+            iEle.classList.remove('fa-lock-open');
+            iEle.classList.add('fa-lock');
+            textArea.setAttribute('contenteditable', 'false');
+        }
+    });
+}
+function handleColerPriority(ticketCont) {
+    console.log(ticketCont.children[0].classList);
+    const ticketColorEle = ticketCont.children[0];
+    ticketColorEle.addEventListener('click', () => {
+        const currentColor = ticketColorEle.classList[1];
+        ticketColorEle.classList.remove(currentColor);
+        console.log(currentColor);
+        ticketColorEle.classList.add(randomColor(currentColor));
+        console.log(ticketCont.children[0].classList);
+    });
+}
+function randomColor(currentColor) {
+    let j = Math.floor(Math.random() * 4);
+    for (let i = 0; i < colors.length; i++) {
+        if (currentColor !== colors[j]) {
+            return colors[j];
+        } else {
+            j = Math.floor(Math.random() * 4);
+            i--;
+        }
+    }
 }
